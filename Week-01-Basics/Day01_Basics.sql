589cbbd114d7cfc3f9633b223abfa2fb04c503be
akash-sivakumar <akash1192006@gmail.com>
2025-12-12 20:38:41 +0530

    Create database and students table plus basic query

---
+++ Week-01-Basics/Day01_Basics.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE day1_practice;
+USE day1_practice;
+
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(50),
+    age INT
+);
+
+INSERT INTO students VALUES
+(1, 'Akash', 20),
+(2, 'Rahul', 22),
+(3, 'Sita', 19);
